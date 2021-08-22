81d03e8c472c075ab78207010546e65c64290d68
Porifly <ataqueomega07@gmail.com>
2021-08-21 22:17:20 -0300

    Manejador de usuarios get datos

--- coronaTicketsUy/src/main/java/presentacion/ManejadorUsuarios.java
+++ coronaTicketsUy/src/main/java/presentacion/ManejadorUsuarios.java
@@ -15,6 +15,7 @@ import javax.persistence.*;
  */
 public class ManejadorUsuarios 
 {
+    //Get la lista de usuarios
     public List<DtUsuario> getDatos()
     {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("PERSISTENCIA");
