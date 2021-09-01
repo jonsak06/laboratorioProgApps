e72934bf7510ba874d1558515478f912e8ef7a81
osiris <osiris@osiris-VirtualBox>
2021-08-31 22:11:08 -0300

    casos de uso 6 y 7 completos

--- coronaTicketsUy/src/main/java/presentacion/AltaFuncion.java
+++ coronaTicketsUy/src/main/java/presentacion/AltaFuncion.java
@@ -343,6 +343,7 @@ public class AltaFuncion extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -371,6 +372,8 @@ public class AltaFuncion extends javax.swing.JFrame {
                     else{
                     Fabrica.getCtrlEspectaculos().crearFuncion(ComboBoxEsp.getSelectedItem().toString(), fun, artConf);
                     JOptionPane.showMessageDialog(null, "Funcion creada");
+                    artConf.clear();
+                    dispose();
                     }
                     }
        
