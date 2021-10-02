2fce471b40273553c8cf88d114bb1bf5ef3f0c88
jsmatematica <jsmatematica@gmail.com>
2021-10-02 16:27:13 -0300

    Algunas actualizaciones

--- coronaTicketsUy/src/main/java/root/entidades/Categoria.java
+++ coronaTicketsUy/src/main/java/root/entidades/Categoria.java
@@ -13,6 +13,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
 /**
@@ -20,6 +22,9 @@ import javax.persistence.OneToMany;
  * @author julio
  */
 @Entity
+@NamedQueries({
+    @NamedQuery(name = "Categoria.findAll", query = "SELECT c FROM Categoria c"),
+    @NamedQuery(name = "Categoria.findByNombre", query = "SELECT c FROM Catgoria c WHERE c.nombre = :nombre")})
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
